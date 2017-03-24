1882cf9097c9f02698efdb84c3fbea0df9dfcc57
Cong Zhao <cong@revinate.com>
2017-03-24 13:35:23 -0700

    revert the name switch

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 module.exports = {
-  'commonDefinitions': require('./ship-sdk/schemata/common-definitions.json'),
-  'guestStayListResultSchema': require('./ship-sdk/schemata/guest-stay-list-result-schema.json'),
-  'guestStayListSchema': require('./ship-sdk/schemata/guest-stay-list-schema.json'),
-  'guestStayResultSchema': require('./ship-sdk/schemata/guest-stay-result-schema.json'),
-  'guestStaySchema': require('./ship-sdk/schemata/guest-stay-schema.json'),
-  'profileSchema': require('./ship-sdk/schemata/profile-schema.json'),
-  'resultSchema': require('./ship-sdk/schemata/result-schema.json'),
-  'simpleGuestStayListSchema': require('./ship-sdk/schemata/simple-guest-stay-list-schema.json'),
-  'simpleGuestStaySchema': require('./ship-sdk/schemata/simple-guest-stay-schema.json')
+  'common-definitions': require('./ship-sdk/schemata/common-definitions.json'),
+  'guest-stay-list-result-schema': require('./ship-sdk/schemata/guest-stay-list-result-schema.json'),
+  'guest-stay-list-schema': require('./ship-sdk/schemata/guest-stay-list-schema.json'),
+  'guest-stay-result-schema': require('./ship-sdk/schemata/guest-stay-result-schema.json'),
+  'guest-stay-schema': require('./ship-sdk/schemata/guest-stay-schema.json'),
+  'profile-schema': require('./ship-sdk/schemata/profile-schema.json'),
+  'result-schema': require('./ship-sdk/schemata/result-schema.json'),
+  'simple-guest-stay-list-schema': require('./ship-sdk/schemata/simple-guest-stay-list-schema.json'),
+  'simple-guest-stay-schema': require('./ship-sdk/schemata/simple-guest-stay-schema.json')
 };
